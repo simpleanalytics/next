56bba4715cdabe00565d72aee2474103dcc2633a
Jean P.D. Meijer <jean@analoginterface.io>
2024-12-18 14:31:58 +0100

    fix(package): add @simpleanalytics/client to tsup

--- packages/analytics/tsup.config.ts
+++ packages/analytics/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 import { exec } from "node:child_process";
 
 export default defineConfig((options) => ({
-  entry: ["src/index.ts"],
+  entry: ["src/index.ts", "src/client/index.ts"],
   splitting: false,
   treeshake: true,
   sourcemap: true,
