ee31d1955351d4f6a7bb80c645ecc1f0f8f61949
Jean P.D. Meijer <jean@analoginterface.io>
2024-12-18 15:34:35 +0100

    fix(package): retain "use client" directive during compilation

--- packages/analytics/src/client/index.tsx
+++ packages/analytics/src/client/index.tsx
@@ -1,2 +1,4 @@
+"use client";
+
 export { SimpleAnalytics } from "./simple-analytics";
 export { useSimpleAnalytics } from "./use-simple-analytics";
--- packages/analytics/tsup.config.ts
+++ packages/analytics/tsup.config.ts
@@ -1,16 +1,27 @@
 import { defineConfig } from "tsup";
 import { exec } from "node:child_process";
+import { preserveDirectivesPlugin } from 'esbuild-plugin-preserve-directives';
 
-export default defineConfig((options) => ({
-  entry: ["src/index.ts", "src/client/index.ts", "src/plugin/index.ts"],
-  splitting: false,
-  treeshake: true,
-  sourcemap: true,
-  dts: true,
-  clean: true,
-  outDir: "dist", // Where you want your compiled files to live
-  onSuccess: async () => {
-    exec("tsc --emitDeclarationOnly");
-  },
-  ...options,
-}));
+export default defineConfig((options) => ([
+  {
+    entry: ["src/index.ts", "src/client/index.tsx", "src/plugin/index.ts"],
+    splitting: false,
+    treeshake: true,
+    sourcemap: true,
+    dts: true,
+    clean: true,
+    outDir: "dist", // Where you want your compiled files to live
+    onSuccess: async () => {
+      exec("tsc --emitDeclarationOnly");
+    },
+    metafile: true,
+    esbuildPlugins: [
+      preserveDirectivesPlugin({
+        directives: ['use client', 'use strict'],
+        include: /\.(js|ts|jsx|tsx)$/,
+        exclude: /node_modules/,
+      }),
+    ],
+    ...options,
+  }
+]));
