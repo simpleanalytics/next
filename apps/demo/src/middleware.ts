90e24ca91119c1d2b4c45f93a950c9ffbc84bee6
Jean P.D. Meijer <jean@analoginterface.io>
2024-12-18 16:06:12 +0100

    feat(demo): add event tracking in middleware

---
+++ apps/demo/src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server'
+import type { NextFetchEvent, NextRequest } from 'next/server'
+import { simpleAnalytics } from "@simpleanalytics/next/server";
+
+const client = simpleAnalytics({
+  hostname: "simpleanalytics-next.vercel.app",
+});
+
+export function middleware(req: NextRequest, event: NextFetchEvent) {
+  event.waitUntil(
+    client.trackEvent("hello from middleware", {
+      path: req.nextUrl.pathname,
+    })
+  )
+ 
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
+}
