d3b9de8a0a459858f382e2a12214f36bcacd8198
Jean P.D. Meijer <jean@analoginterface.io>
2024-12-18 02:21:08 +0100

    chore: initial commit

---
+++ packages/analytics/tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "tsup";
+import { exec } from "node:child_process";
+
+export default defineConfig((options) => ({
+  entry: ["src/index.ts"],
+  splitting: false,
+  treeshake: true,
+  sourcemap: true,
+  clean: true,
+  outDir: "dist", // Where you want your compiled files to live
+  onSuccess: async () => {
+    exec("tsc --emitDeclarationOnly");
+  },
+  ...options,
+}));
