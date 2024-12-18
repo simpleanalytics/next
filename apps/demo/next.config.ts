5de4781a6c5d0673e514162db7dd2d04728439d4
Jean P.D. Meijer <jean@analoginterface.io>
2024-12-18 15:40:29 +0100

    fix(demo): use vercel production url

--- apps/demo/next.config.ts
+++ apps/demo/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 import withSimpleAnalytics from "@simpleanalytics/next/plugin";
 
 const nextConfig: NextConfig = withSimpleAnalytics({
-  domain: process.env.VERCEL_URL!,
+  domain: process.env.VERCEL_PROJECT_PRODUCTION_URL!,
 });
 
 export default nextConfig;
