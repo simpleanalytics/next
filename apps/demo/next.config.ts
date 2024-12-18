4316cf7911f96a650d506bcd6556f65339396daa
Jean P.D. Meijer <jean@analoginterface.io>
2024-12-18 14:43:55 +0100

    fix(demo): install Next.js config plugin

--- apps/demo/next.config.ts
+++ apps/demo/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+import withSimpleAnalytics from "@simpleanalytics/next/plugin";
+
+const nextConfig: NextConfig = withSimpleAnalytics();
 
 export default nextConfig;
