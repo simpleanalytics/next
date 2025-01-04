4b8d8cfbf202cac9adf08faa91559807b57d7bf9
Jean P.D. Meijer <jean@analoginterface.io>
2025-01-04 20:41:22 +0100

    feat(server): add timezone parsing for Vercel and Amazon CloudFront

--- packages/analytics/src/server/client-hints.ts
+++ packages/analytics/src/server/client-hints.ts
@@ -12,11 +12,16 @@ export function parseLanguage(headers: Headers) {
   return headers.get("Sec-CH-Lang") ?? headers.get("Lang");
 }
 
+export function parseTimezone(headers: Headers) {
+  return headers.get("X-Vercel-IP-Timezone") ?? headers.get("CloudFront-Viewer-Time-Zone");
+}
+
 export function collectFromHeaders(headers: Headers, ignoredMetrics: IgnoredMetrics) {
   return {
     viewport_width: !ignoredMetrics.viewportSize ? parseViewportWidth(headers) : undefined,
     viewport_height: !ignoredMetrics.viewportSize ? parseViewportHeight(headers) : undefined,
 
     language: !ignoredMetrics.language ? parseLanguage(headers) : undefined,
+    timezone: !ignoredMetrics.timezone ? parseTimezone(headers) : undefined,
   }
 }
