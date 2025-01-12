e1656aa077e3b7aa3e2f6b935e75a5f60077c632
Jean P.D. Meijer <jean@analoginterface.io>
2025-01-12 01:35:29 +0100

    refactor(docs): use variable for Inter font

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -5,11 +5,12 @@ import type { ReactNode } from "react";
 
 const inter = Inter({
   subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={`${inter.variable}`} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
