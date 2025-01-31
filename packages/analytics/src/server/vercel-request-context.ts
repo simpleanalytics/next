47885116b283a8e67162d5cb5b1f06d91d7e716b
Jean P.D. Meijer <jean@analoginterface.io>
2025-01-31 19:00:48 +0100

    chore(server): add Vercel request context helpers

---
+++ packages/analytics/src/server/vercel-request-context.ts
@@ -0,0 +1,21 @@
+export interface VercelRequestContextValue {
+  headers: Record<string, string | undefined>;
+  url: string;
+  [key: symbol]: unknown;
+}
+
+type VercelRequestContext = {
+  get(): VercelRequestContextValue | undefined
+}
+
+const symbol = Symbol.for("@vercel/request-context");
+
+interface Global {
+  [symbol]?: VercelRequestContext;
+}
+
+export function getVercelRequestContext(): VercelRequestContextValue | undefined {
+  const requestContext = (globalThis as Global)[symbol];
+
+  return requestContext?.get();
+}
