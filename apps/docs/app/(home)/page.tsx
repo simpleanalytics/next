f6a35ceef43341fa20240cb37f6b9aebbca9f5e0
Jean P.D. Meijer <jean@analoginterface.io>
2025-02-01 13:36:46 +0100

    chore(docs): disable landing page, redirect to docs

--- apps/docs/app/(home)/page.tsx
+++ apps/docs/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { DynamicCodeBlock } from "fumadocs-ui/components/dynamic-codeblock";
 import { Tab, Tabs } from "fumadocs-ui/components/tabs";
+import { redirect } from "next/navigation";
 
 interface PackageCommandProps {
   code: string;
@@ -22,37 +23,39 @@ function PackageCommand({ code }: PackageCommandProps) {
 }
 
 export default function HomePage() {
-  return (
-    <main className="flex flex-1 flex-col justify-center text-center container items-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{" "}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
-        >
-          /docs
-        </Link>{" "}
-        and see the documentation.
-      </p>
+  redirect("/docs");
 
-      <Tabs className="max-w-96 w-full" items={['npm', 'pnpm', 'yarn', 'bun', "deno"]}>
-        <Tab value="npm">
-          <PackageCommand code="npm i @simpleanalytics/next" />
-        </Tab>
-        <Tab value="pnpm">
-          <PackageCommand code="pnpm i @simpleanalytics/next" />
-        </Tab>
-        <Tab value="yarn">
-          <PackageCommand code="yarn add @simpleanalytics/next" />
-        </Tab>
-        <Tab value="bun">
-          <PackageCommand code="bun add @simpleanalytics/next" />
-        </Tab>
-        <Tab value="deno">
-          <PackageCommand code="deno add npm:@simpleanalytics/next" />
-        </Tab>
-      </Tabs>
-    </main>
-  );
+  // return (
+  //   <main className="flex flex-1 flex-col justify-center text-center container items-center">
+  //     <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+  //     <p className="text-fd-muted-foreground">
+  //       You can open{" "}
+  //       <Link
+  //         href="/docs"
+  //         className="text-fd-foreground font-semibold underline"
+  //       >
+  //         /docs
+  //       </Link>{" "}
+  //       and see the documentation.
+  //     </p>
+  //
+  //     <Tabs className="max-w-96 w-full" items={['npm', 'pnpm', 'yarn', 'bun', "deno"]}>
+  //       <Tab value="npm">
+  //         <PackageCommand code="npm i @simpleanalytics/next" />
+  //       </Tab>
+  //       <Tab value="pnpm">
+  //         <PackageCommand code="pnpm i @simpleanalytics/next" />
+  //       </Tab>
+  //       <Tab value="yarn">
+  //         <PackageCommand code="yarn add @simpleanalytics/next" />
+  //       </Tab>
+  //       <Tab value="bun">
+  //         <PackageCommand code="bun add @simpleanalytics/next" />
+  //       </Tab>
+  //       <Tab value="deno">
+  //         <PackageCommand code="deno add npm:@simpleanalytics/next" />
+  //       </Tab>
+  //     </Tabs>
+  //   </main>
+  // );
 }
