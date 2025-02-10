2ac5f577db0edf0256df310edfd835d9a2bb9a9e
Jean P.D. Meijer <jean@analoginterface.io>
2025-02-10 17:03:20 +0100

    chore(server): remove experimental flag for middleware

--- packages/analytics/src/middleware/routes.ts
+++ packages/analytics/src/middleware/routes.ts
@@ -3,10 +3,6 @@ interface RoutesModule {
 }
 
 export async function isExistingRoute(path: string) {
-  if (process.env.EXPERIMENTAL_ANALYTICS_MIDDLEWARE !== "1") {
-    return true;
-  }
-
   // @ts-expect-error
   const { pattern }: RoutesModule = await import("/.simpleanalytics/routes.js");
 
